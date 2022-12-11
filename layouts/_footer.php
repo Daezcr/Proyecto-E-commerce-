85d06f7163a2bbf5a3e5925ea024f2e788caa62f
Daezcr <112747617+Daezcr@users.noreply.github.com>
2022-12-11 16:32:44 -0600

    Add files via upload

---
+++ layouts/_footer.php
@@ -0,0 +1,20 @@
+<footer>
+	<div class="body-footer">
+		<div class="div-flex mb20">
+			<div class="part">
+				<br>
+				<h4>QUIENES SOMOS??</h4>
+				<p>SOMOS LA EMPRESA DH STORE<BR>EN DONDE ENCONTRARAS PRODUCTOS SORPRENDENTES PARA DISFUTRAR:) </p>
+			</div>
+			<div class="part">
+				<h4 class="mb10">Redes sociales</h4>
+				<a href="" target="_blank">Facebook: DANIEL IC/HEIDI/EMANUEL MORENO</a>
+				<br>
+				<a href="" target="_blank">WhatsApp: 272-248-6640/2721902309 </a>
+				<br>
+				<a href="" target="_blank">Fundadores: DANIEL CANO/HEIDI YELITZA/EMANUEL MORENO </a>
+			</div>
+		</div>
+		<center><h5>STORE DH - Derechos reservador 2022-2025</h5></center>
+	</div>
+</footer>
